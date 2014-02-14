6d27b095627a70bc045f0a7ac214a0362e887646
Shu Ghosh <shu.ghosh@gmail.com>
2014-02-14 12:54:38 -0500

    Add image upload with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,9 @@
 class Pin < ActiveRecord::Base
+	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.8.8-Q16'
+
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
 end
+
