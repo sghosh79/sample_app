24fd22666fab133be576ea7b36ef5326ff56230b
Shu Ghosh <shu.ghosh@gmail.com>
2014-02-16 15:51:59 -0500

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,5 +5,9 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
+	validates :image, presence: true
+	validates :description, presence: true
+
+
 end
 
